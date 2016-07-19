d5d1710b39c7acb0dafaefb7ee0d3d2f2086a687
Daniel Ho <danielho54@gmail.com>
2016-07-19 19:25:32 +0000

    poop

--- that_was_easy/main.js
+++ that_was_easy/main.js
@@ -7,7 +7,9 @@ $("#Easy").on("click", sayThatWasEasy);
 $(document).keypress(delegateKeypress);
 
 function delegateKeypress(event){
+    event.preventDefault();
+    console.log(event.charCode);
     if(event.charCode==32){
-        $("#Easy").trigger("click");
+        sayThatWasEasy();
     }
 }  

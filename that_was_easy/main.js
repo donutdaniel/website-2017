675400f388fd3840707170fe4ea213641403a9be
Daniel Ho <danielho54@gmail.com>
2016-07-19 18:50:47 +0000

    css

--- that_was_easy/main.js
+++ that_was_easy/main.js
@@ -3,4 +3,4 @@ var thatWasEasy = new Audio("that_was_easy.mp3");
 function sayThatWasEasy(){
     thatWasEasy.play();
 }
-$("#easy").on("click", sayThatWasEasy);
+$("#Easy").on("click", sayThatWasEasy);

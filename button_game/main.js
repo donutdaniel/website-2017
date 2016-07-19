d2e8ce7b9d71bee57d8a4dc0740e2fc22d99723c
Daniel Ho <danielho54@gmail.com>
2016-07-19 22:57:42 +0000

    poop

--- button_game/main.js
+++ button_game/main.js
@@ -1,8 +1,24 @@
-var count = 10;
+var count = 1;
+var increment = 1;
+var multiplier = 1;
 
 function updateDonut(){
-    count++;
-    document.getElementById("count").innerHTML = count;
+    count=count+(increment*multiplier);
+    document.getElementById("count").innerHTML = Math.round(count);
 }
 
+function increaseMultiplier(){
+    multiplier=multiplier+.5;
+    document.getElementById("mutliplier").innerHTML = multiplier;
+}
+
+function increaseIncrement(){
+    increment++;
+    document.getElementById("increment").innerHTML = increment;
+}
+
+setInterval(updateDonut,1000);
+
+$("#incI").on("click", increaseIncrement);
+$("#incM").on("click", increaseMultiplier);
 $("#donut").on("click", updateDonut);

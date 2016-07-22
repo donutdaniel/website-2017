f51e37c786c8d1f015f8ef0301a39a86f57c2893
Daniel Ho <danielho54@gmail.com>
2016-07-22 22:47:03 +0000

    poop

--- dodge_or_die/game.js
+++ dodge_or_die/game.js
@@ -1,23 +1,27 @@
 var player;
-var playerImage;
-var enemy;
-var enemyImage;
+var enemies;
+var enemyTypeC;
 var isGameOver;
-var backgroundImage;
 var gravity;
-var normal = get(0,0);
+var obstacles;
+var deleteID;
 
 function setup(){
     createCanvas(1400,700);
+    enemies = new Group();
     player = createSprite(width/2,height-25,50,50);
-    enemy = createSprite(random(5,width-5),-15,30,30);
+    for(var i=0;i<10;i++){
+        var enemy = createSprite(random(5,width-5),-15,30,30);
+        enemies.add(enemy);
+    }
     gravity= 0.4; 
     isGameOver = false;
+    obstacles = new Group();
 }
 
 function create(){
     player = createSprite(width/2,height-25,50,50);
-    enemy = createSprite(random(5,width-5),-15,30,30);
+    // enemy = createSprite(random(5,width-5),-15,30,30);
     isGameOver = false;
 }
 
@@ -26,9 +30,11 @@ function draw(){
     if(isGameOver){
         gameOver();
     } else {
-    if(enemy.position.y>height+50){ // wrap
-        enemy.position.y=0;
-        enemy.position.x=random(5,width-5)
+    for(var i=0;i<enemies.length;i++){
+    if(enemies[i].position.y>height+50){ // wrap
+        enemies[i].position.y=0;
+        enemies[i].position.x=random(5,width-5)
+    }
     }
     
     // if(keyDown(87))
@@ -40,24 +46,19 @@ function draw(){
     if(keyIsDown(68)&&player.position.x<width-25)
         player.position.x+=3;
         
-    if(keyIsDown(32)&&(player.velocity.y===0)){ //jump
-        player.velocity.y=-5;
+    jump();
+    
+    for(var i=0;i<enemies.length;i++){
+    enemies[i].position.y+=1.5;
     }
-    if(player.velocity.y<0){
-        player.velocity.y+=gravity;
     }
-    if(player.position.y>height-25){
-        player.velocity.y=0;
-    }
-    enemy.position.y+=1.5;
-    drawSprites();
-    }
-    if (enemy.overlap(player)) {
+    if (enemies.overlap(player,storeID)) {
+        player.setVelocity(0,0)
         isGameOver=true;
     }
     
-    player.collide(player);
-        
+    player.collide(obstacles,stopFall);
+    drawSprites();
 }
 
 function gameOver(){
@@ -75,6 +76,29 @@ function gameOver(){
     //     enemy.position.x=random(5,width-5);
     // }
     if(keyIsPressed){
-        setup();
+        obstacles.add(player);
+        obstacles.add(enemies[deleteID]);
+        create();
     }
+}
+
+function jump(){
+    if(keyIsDown(32)&&player.velocity.y===0){
+        player.velocity.y=-10;
+    }
+    if(player.velocity.y!=0||(!player.overlap(obstacles))){
+        player.velocity.y=player.velocity.y+gravity;
+    }
+    if(player.position.y>height-25){
+        player.velocity.y=0;
+        player.position.y=height-25;
+    }
+}
+
+function stopFall(){
+    player.velocity.y=0;
+}
+
+function storeID(objA,objB){
+    deleteID=objB.get(i);
 }

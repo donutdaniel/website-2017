f2b985ce948c45bc9a17aca7dcbe2fe66bcfd25a
Daniel Ho <danielho54@gmail.com>
2016-07-28 23:11:00 +0000

    poop

---
+++ dank_facts/main.js
@@ -0,0 +1,2 @@
+Twilio.sendMessage("510-656-6901", "You just subscribed to Dank Facts");
+Twilio.sendMessage("415-744-4926", "You just subscribed to Dank Facts");
--- magoose_game/main.js
+++ magoose_game/main.js
@@ -1,26 +1,49 @@
 var player;
 var playerImage;
+var enemies;
+var enemiesNum = 25;
+var ratImage;
+var leafImage;
 var backgroundImage;
 var gravity;
+var score = 0;
+var canvas;
+var checkBound=0;
 
 function preload() {
     playerImage = loadImage("mongoose.png");
-    backgroundImage = loadImage("background.png")
+    backgroundImage = loadImage("background.png");
+    ratImage = loadImage("rat.png");
+    leafImage = loadImage("leaf.png");
 }
 
 function setup() {
-    createCanvas(1000, 600);
+    canvas = createCanvas(windowWidth - 20, windowHeight - 20);
     player = createSprite(playerImage.width / 2, height - (playerImage.height / 2), 0, 0);
     player.addImage(playerImage);
-    gravity = 0.4;
+    enemies = new Group();
+    gravity = 0.;
+
+    for (var i = 0; i < enemiesNum; i++) {
+        var rat = createSprite(width / 2 + (Math.random() * 5000), ((Math.random() * 0.6) + 0.3) * height, 0, 0);
+        rat.addImage(ratImage);
+        enemies.push(rat);
+    }
 }
 
 function draw() {
-    background(10,150,200);
+    background(170, 200, 250);
+    //background(backgroundImage);
     playerMove();
     jump();
+    enemies.overlap(player, updateScore);
     camera.position.x = player.position.x + (width / 4);
     drawSprites();
+    text("Score: " + score, camera.position.x - (width / 2), 15);
+
+    if ((score >= enemiesNum)&&(checkBound===0)) {
+        lvlUp();
+    }
 }
 
 function playerMove() {
@@ -36,11 +59,29 @@ function playerMove() {
 function jump() {
     player.velocity.y += gravity;
     if ((keyIsDown(32) || keyIsDown(UP_ARROW)) && player.velocity.y === gravity) {
-        player.velocity.y = -14;
+        player.velocity.y = -18;
     }
 
     if (player.position.y > height - (playerImage.height / 2)) {
         player.velocity.y = 0;
         player.position.y = height - (playerImage.height / 2);
+    }
+}
+
+function updateScore(spriteA, spriteB) {
+    score++;
+    spriteA.remove();
+}
+
+function lvlUp() {
+    checkBound++;
+    playerImage = loadImage("giraffe.png");
+    player.addAnimation("lvl2", playerImage);
+    player.changeAnimation("lvl2");
+    enemies=new Group();
+    for (var i = 0; i < enemiesNum; i++) {
+        var leaf = createSprite((player.position.x/2) + (Math.random() * 50), ((Math.random() * 0.6) + 0.3) * height, 0, 0);
+        leaf.addImage(leafImage);
+        enemies.push(leaf);
     }
 }

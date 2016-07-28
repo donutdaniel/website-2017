8fff0633c024b7674beca2f6fe28a2a28da0fdf0
Daniel Ho <danielho54@gmail.com>
2016-07-28 23:41:18 +0000

    poop

--- magoose_game/main.js
+++ magoose_game/main.js
@@ -59,7 +59,7 @@ function playerMove() {
 function jump() {
     player.velocity.y += gravity;
     if ((keyIsDown(32) || keyIsDown(UP_ARROW)) && player.velocity.y === gravity) {
-        player.velocity.y = -18;
+        player.velocity.y = -16;
     }
 
     if (player.position.y > height - (playerImage.height / 2)) {
@@ -78,9 +78,10 @@ function lvlUp() {
     playerImage = loadImage("giraffe.png");
     player.addAnimation("lvl2", playerImage);
     player.changeAnimation("lvl2");
+    player.setCollider("giraffe",0,0,playerImage.width,playerImage.height);
     enemies=new Group();
     for (var i = 0; i < enemiesNum; i++) {
-        var leaf = createSprite((player.position.x/2) + (Math.random() * 50), ((Math.random() * 0.6) + 0.3) * height, 0, 0);
+        var leaf = createSprite((player.position.x+(width/2)) + (Math.random() * 5000), ((Math.random() * 0.6) + 0.3) * height, 0, 0);
         leaf.addImage(leafImage);
         enemies.push(leaf);
     }
